f5c7aa5541a26197012061ad9aa9c0e614c20559
Shrenik Patel <63368512+shrenikpatel17@users.noreply.github.com>
2025-02-14 21:26:42 -0800

    added tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        'grad-light': '#F1F6F0',
+        'grad-dark' : '#C1D1BE',
+        'text-green': '#044723',
+        'hover-dark-green':'#003318',
+        'dark-green': '#547A51',
+        'light-color': '#FEFFFC',
+        'hover-light':'#f2f9f0',
+      },
     },
   },
   plugins: [],
